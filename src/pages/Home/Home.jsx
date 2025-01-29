96e2da24dd76cba30a55e9e6e415fcfd7c8d95b6
mohiuddinahmed121 <mohiuddinahmed121@gmail.com>
2025-01-29 19:22:47 +0600

    Banner section added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import Navbar from "../../pages/Shared/Navbar/Navbar";
 import Header from "../../pages/Shared/Header/Header";
+import Banner from "../../pages/Banner/Banner";
 
 const Home = () => {
    return (
       <div>
          <Header></Header>
          <Navbar></Navbar>
+         <Banner></Banner>
       </div>
    );
 };
